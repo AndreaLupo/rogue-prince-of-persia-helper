409043d7d9da645f7f67f188d24d40306423d2b2
Andrea Lupo <lupo.andrea.93@gmail.com>
2024-06-30 09:59:23 +0200

    Add page with specific build

---
+++ src/routes/builds/[build]/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import { page } from '$app/stores';
+    import BuildDetail from '$lib/components/BuildDetail.svelte';
+    import { getAllBuilds } from '../../../helpers/build-generator';
+    import type { Build } from '../../../types';
+
+    let build: Build;
+
+    page.subscribe(( page => {
+        const buildId = page.params.build;
+
+        const builds = getAllBuilds();
+
+        for(const currentBuild of builds) {
+            if(currentBuild.hash === buildId) {
+                build = currentBuild;
+                break;
+            }
+        }
+
+    }));
+</script>
+
+<BuildDetail {build}></BuildDetail>
